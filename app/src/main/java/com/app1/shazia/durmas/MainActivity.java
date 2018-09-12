1e1443854cd2aa003764f4d08f4f79d9bfd9835d
Shazia <shazia.nustceme@gmail.com>
2018-09-12 22:57:43 +0500

    Durmas project

---
+++ app/src/main/java/com/app1/shazia/durmas/MainActivity.java
@@ -0,0 +1,29 @@
+package com.app1.shazia.durmas;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        new Handler(  ).postDelayed( new Runnable() {
+            @Override
+            public void run (){
+                Intent categoryIntent = new Intent(MainActivity.this, categoryActivity.class  );
+                startActivity( categoryIntent );
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/app1/shazia/durmas/categoryActivity.java
@@ -0,0 +1,30 @@
+package com.app1.shazia.durmas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class categoryActivity extends AppCompatActivity {
+
+
+    Button facebookButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_category );
+        facebookButton = (Button) findViewById( R.id.button);
+        facebookButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent categoryIntent = new Intent(categoryActivity.this,CategorySelection.class );
+                startActivity( categoryIntent );
+
+            }
+        } );
+
+    }
+}
